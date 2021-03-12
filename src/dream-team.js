af30c4d08420b5e9a5c56df77139d16661a3f847
alexsharich <alexsharich@gmail.com>
2021-03-12 16:05:25 +0300

    solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arr) {
+ 
+    let result = []
+    let res = []
+    let uppers = ['Q','W','E','R','T','Y','U','I','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+    for (let elem of arr){
+    if(typeof(elem) === 'string'){
+    result.push(elem.slice(0,1))
+    }
+    }
+    for (let elem of result){/*add symblos uppercase*/
+    if(uppers.indexOf(elem) !== -1){
+    res.push(elem)
+    }
+    }
+    
+    return(res.sort().join(''))
+    }
+    
+   
+
