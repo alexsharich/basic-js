3c8ae9cd92dcc047289901389afe31d24d02fc43
alexsharich <alexsharich@gmail.com>
2021-03-14 14:18:48 +0300

    sol

--- src/what-season.js
+++ src/what-season.js
@@ -1,17 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(month) {
-  if (date === undefined) return 'Unable to determine the time of year!';
-  if (Object.prototype.toString.call(date) !== "[object Date]") throw Error;
-  const month = date.getMonth()
-  switch(true) {
-    case month >= 2 && month <= 4:
-      return 'spring'
-    case month >= 5 && month <= 7:
-      return 'summer'
-    case month >= 8 && month <= 10:
-      return 'autumn'
-    default:
-      return 'winter'
-  }
+  
 };
